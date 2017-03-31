1638724f8ff2ffc2b7ac704df04a1b19ff5f6efa
Ali Abdulkadir <autostart.ini@gmail.com>
2017-04-01 00:42:35 +0300

    added verbose option to autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-autoreconf -i -W all
+
+autoreconf -vifW all
+
+
+# aclocal --force --warnings=all -I m4
+# libtoolize --copy --force
+# autoconf --force --warnings=all
+# autoheader --force --warnings=all
+# automake --add-missing --copy --force-missing --warnings=all
+
